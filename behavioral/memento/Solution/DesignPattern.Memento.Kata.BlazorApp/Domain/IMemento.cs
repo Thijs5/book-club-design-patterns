58198df7d5ca2fad6a8138deaee74d9d69a43c65
Bram Hendrickx <bram_hendrickx@hotmail.com>
2020-12-09 20:37:41 +0100

    Added memento

---
+++ behavioral/memento/Solution/DesignPattern.Memento.Kata.BlazorApp/Domain/IMemento.cs
@@ -0,0 +1,38 @@
+﻿namespace DesignPattern.Memento.Kata.BlazorApp.Domain
+{
+    public interface IMemento
+    {
+        string GetName();
+        Tile[] GetTiles();
+        string GetTheme();
+    }
+
+    internal class MapLayoutMemento : IMemento
+    {
+        private readonly string _name;
+        private readonly Tile[] _tiles;
+        private readonly string _theme;
+
+        public MapLayoutMemento(string name, Tile[] tiles, string theme)
+        {
+            _name = name;
+            _tiles = tiles;
+            _theme = theme;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public Tile[] GetTiles()
+        {
+            return _tiles;
+        }
+
+        public string GetTheme()
+        {
+            return _theme;
+        }
+    }
+}
--- behavioral/memento/readme.md
+++ behavioral/memento/readme.md
@@ -1,1 +1,9 @@
-# TODO
+# Memento
+
+In the Kata folder, you can find the base to get started. The solution is found in the solution folder.
+
+## Intro
+
+We're a game-map company using a blazor app. sadly, we forgot an undo function ... doh. So let's apply the memento pattern to fix this!.
+
+Do note, due to blazor and the use of a foreach loop inside the svg to show our map, the UI isn't updated correctly. however, the code should work in the solution folder if you set a breakpoint.
