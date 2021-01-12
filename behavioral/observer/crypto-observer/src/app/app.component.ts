66e16d23c83d3573c9bd54111db59faf96b6f76b
Casper Kurkiewicz <casper.kurkiewicz@cegeka.com>
2021-01-12 23:11:04 +0100

    Solution

--- behavioral/observer/crypto-observer/src/app/app.component.ts
+++ behavioral/observer/crypto-observer/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { BitCoin } from './models/crypto-currencies/bitcoin';
+import { Person } from './models/observers/person';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'crypto-observer';
+export class AppComponent implements OnInit {
+  bitcoin: BitCoin;
+  persons: Person[] = [];
+
+  ngOnInit(): void {
+    this.bitcoin = new BitCoin(10);
+    const frank = new Person('Frank');
+    this.bitcoin.attach(frank);
+    const john = new Person('John');
+    this.bitcoin.attach(john);
+    const charlie = new Person('Charlie');
+    this.bitcoin.attach(charlie);
+
+    this.persons.push(frank);
+    this.persons.push(john);
+    this.persons.push(charlie);
+
+    interval(2000)
+      .subscribe({
+        next: () => this.bitcoin.price = Math.floor(Math.random() * 100)
+      });
+  }
 }
